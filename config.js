a70e1469a01310e45597928f8e6f1e02d880ddd4
otieno7 <162002559+otieno7@users.noreply.github.com>
2024-03-03 09:43:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+254 759 439191', 'ATG', true],
+  ['+254 759 439191', 'ATG', true], 
   [''] 
 ] //Number of owners
 
